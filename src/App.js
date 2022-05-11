92a84ad3db7fa7fb9b50828305c9e225938633c8
Najib-Hebrawi <najiphebrawi@hotmail.com>
2022-05-11 10:44:28 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,17 +4,39 @@ import React, { Component, useState } from 'react'
 
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    
+  }
+
+
+
+
+
+   decrement= ()=>{
+    this.setState({
+      count: this.state.count -1
+    })
+  }
+   increment= ()=>{
+   this.setState({
+     count: this.state.count +1
+   })
+  }
 
   render() {
-
-
     return (
-
-      
+  
       <div>
+     
+     <h1> welcome dude </h1>
+     <p>the count is {this.state.count}</p>
 
-        
-
+     <button onClick={this.decrement} style={{padding:10}}>-</button>
+     <button onClick={this.increment} style={{padding:10}}>+</button>
 
       </div>
     )
